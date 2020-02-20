d2470c2c4a638166397e68f0079da44a7ae4e7cb
Gregory Havenga <gregory.havenga@gmail.com>
2020-02-20 19:14:59 +0200

    Add more basic initialization

--- parser.rb
+++ parser.rb
@@ -3,8 +3,31 @@ if ARGV.first.nil?
   exit
 end
 
+class Thing
+
+end
+
+class Thing2
+
+end
+
+class Thing3
+
+end
+
+$things = {}
+$things2 = {}
+$things3 = {}
+
+
 filename = ARGV.first
+
+# stream input file
 
 File.foreach(filename).with_index do |line, line_num|
    puts "Line #{line_num}: #{line}"
 end
+
+
+# output results
+
